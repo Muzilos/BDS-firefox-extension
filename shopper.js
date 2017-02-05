e9525948b903b1e90da2d7f0af247860554b81a3
Alaa Salman <alaa@codedemigod.com>
2017-02-05 18:34:19 +1100

    Modified how the price is displayed next to the original one on the product page. Also made it so that if another display event caused the extension to rerun, it removes the previously displayed price.

--- shopper.js
+++ shopper.js
@@ -1,36 +1,51 @@
 var priceNumRegex = new RegExp("(\\d+\.\\d+)");
 var possibleIds = ["priceblock_ourprice", "priceblock_saleprice"];
+var amznPriceElm = null;
 
 function onError(error) {
-    console.log(`Error: ${error}`);
+    console.error(`AussieShopper : Error ${error}`);
 }
 
-function convertPrice() {
-    var priceElm = null;
-
-    function displayPrice(result) {
+function displayConvertedPrice(result) {
         var audRate = result.exchange_rate || 1;
+        var priceArray = priceNumRegex.exec(amznPriceElm.textContent);
+        
+        if (priceArray) {
+            var audPrice = priceArray[0] * audRate;
+            var displayString = `
+            <div id='as_price' style='color: purple;'>
+                <span>AUD${audPrice.toFixed(2)}</span>
+                <span><a href='gohere.html'>StaticIce</a></span>
+            </div>
+            `;
 
-        if (priceElm) {
-            var priceArray = priceNumRegex.exec(priceElm.textContent);
+            var previouslyDisplayedDiv = document.getElementById("as_price");
 
-            if (priceArray) {
-                var audPrice = priceArray[0] * audRate;
-
-                priceElm.textContent += "/AUD" + audPrice.toFixed(2);
+            if(previouslyDisplayedDiv){
+                previouslyDisplayedDiv.remove();
             }
+
+            amznPriceElm.insertAdjacentHTML('afterend', displayString);
         }
-    }
+        
+}
+
+function lookForPrice() {
+    var priceElm = null;
 
     for (priceIdKey in possibleIds) {
         priceElm = document.getElementById(possibleIds[priceIdKey]);
 
         if (priceElm) {
+            amznPriceElm = priceElm;
             break;
         }
     }
-    var getting = browser.storage.local.get("exchange_rate");
-    getting.then(displayPrice, onError);
+
+    if(priceElm){
+        var getting = browser.storage.local.get("exchange_rate");
+        getting.then(displayConvertedPrice, onError);
+    }
 }
 
-convertPrice();
+lookForPrice();
