ad8036ac75a219d3e450e98715cb6e9e0da04667
Alaa Salman <alaa@codedemigod.com>
2017-02-05 19:16:00 +1100

    Added link to staticice AU product search using the product title. I am sending it the full title of the product which might cause it not to find all products. That's a little weird but I'll try it out for a while and see how it goes.

--- shopper.js
+++ shopper.js
@@ -1,5 +1,6 @@
 var priceNumRegex = new RegExp("(\\d+\.\\d+)");
 var possibleIds = ["priceblock_ourprice", "priceblock_saleprice"];
+var productTitleId = "productTitle";
 var amznPriceElm = null;
 
 function onError(error) {
@@ -7,27 +8,33 @@ function onError(error) {
 }
 
 function displayConvertedPrice(result) {
-        var audRate = result.exchange_rate || 1;
-        var priceArray = priceNumRegex.exec(amznPriceElm.textContent);
-        
-        if (priceArray) {
-            var audPrice = priceArray[0] * audRate;
-            var displayString = `
+    var audRate = result.exchange_rate || 1;
+    var priceArray = priceNumRegex.exec(amznPriceElm.textContent);
+    var productTitleElm = document.getElementById(productTitleId);
+
+    if (priceArray) {
+        var audPrice = priceArray[0] * audRate;
+        var previouslyDisplayedDiv = document.getElementById("as_price");
+        var productTitle = "";
+
+        if (previouslyDisplayedDiv) {
+            previouslyDisplayedDiv.remove();
+        }
+
+        if(productTitleElm){
+            productTitle = encodeURIComponent(productTitleElm.innerText);
+        }
+
+        amznPriceElm.insertAdjacentHTML('afterend', `
             <div id='as_price' style='color: purple;'>
                 <span>AUD${audPrice.toFixed(2)}</span>
-                <span><a href='gohere.html'>StaticIce</a></span>
+                <span>
+                    <a target='_blank' href='http://www.staticice.com.au/cgi-bin/search.cgi?q=${productTitle}'>Find on StaticIce</a>
+                </span>
             </div>
-            `;
+            `);
+    }
 
-            var previouslyDisplayedDiv = document.getElementById("as_price");
-
-            if(previouslyDisplayedDiv){
-                previouslyDisplayedDiv.remove();
-            }
-
-            amznPriceElm.insertAdjacentHTML('afterend', displayString);
-        }
-        
 }
 
 function lookForPrice() {
@@ -42,7 +49,7 @@ function lookForPrice() {
         }
     }
 
-    if(priceElm){
+    if (priceElm) {
         var getting = browser.storage.local.get("exchange_rate");
         getting.then(displayConvertedPrice, onError);
     }
