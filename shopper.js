0036083299c14366f32cedb468596d2435005720
Alaa Salman <alaa@codedemigod.com>
2017-02-12 21:35:19 +1100

    Rewrote DOM manipulation code not to include innerhtml modification as per reviewer comments

--- shopper.js
+++ shopper.js
@@ -25,14 +25,30 @@ function displayConvertedPrice(result) {
             productTitle = encodeURIComponent(productTitleElm.innerText);
         }
 
-        amznPriceElm.insertAdjacentHTML('afterend', `
-            <div id='as_price' style='color: purple;'>
-                <span>AUD${audPrice.toFixed(2)}</span>
-                <span>
-                    <a target='_blank' href='http://www.staticice.com.au/cgi-bin/search.cgi?q=${productTitle}'>Find on StaticIce</a>
-                </span>
-            </div>
-            `);
+        var containingDiv = document.createElement('div');
+        containingDiv.setAttribute('id', 'as_price');
+        containingDiv.setAttribute('style', 'color: purple');
+
+        // price span
+        var priceSpan = document.createElement('span');
+        var priceText = document.createTextNode(`AUD${audPrice.toFixed(2)}`);
+        priceSpan.setAttribute('style', 'margin-right: 5px');
+        priceSpan.appendChild(priceText);
+
+        // link span and its anchor
+        var linkSpan = document.createElement('span');
+        var linkAnchor = document.createElement('a');
+        var anchorText = document.createTextNode('Find on StaticIce');
+        linkAnchor.setAttribute('href', `http://www.staticice.com.au/cgi-bin/search.cgi?q=${productTitle}`);
+        linkAnchor.setAttribute('target', '_blank');
+        linkAnchor.appendChild(anchorText);
+
+        linkSpan.appendChild(linkAnchor);
+
+        // append new elements to the price block
+        containingDiv.appendChild(priceSpan);
+        containingDiv.appendChild(linkSpan);
+        amznPriceElm.parentElement.appendChild(containingDiv);
     }
 
 }
